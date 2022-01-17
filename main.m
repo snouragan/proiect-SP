1df66c54bd31affb284857fe42d6c504de9c530b
Snou Hurricane <ahaiducescu@yahoo.com>
2022-01-17 20:21:28 +0200

    update

--- main.m
+++ main.m
@@ -2,11 +2,15 @@ file = 'Teme proiect SP 421F 422F.xlsx';
 number = 4;
 sheet = 'Sheet1';
 [a2, a1, a0] = readVariables(file, sheet, number);
+t1 = linspace(0, 1);
+t2 = linspace(-1, 3);
+T = [0.5, 1, 2];
 
 % MAIN SIGNAL
-t = linspace(0,1);
-x = a2*t.^2 + a1*t + a0;
+plot(signal_main(a0, a1, a2, t1));
+plot(signal_main(a0, a1, a2, t2));
 
-% SIGNAL xi i = 1-3
-xi = conv(x, dirac(x));
-plot(1:199, xi)
+% 3 periods
+% SIGNAL xi i= 1 3
+
+main_signal_period = 1;
